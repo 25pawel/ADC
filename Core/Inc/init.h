dcef859b31ba742a4ab4e7468d395acd4adbaff1
Pawel Wojcik <25pawel@gmail.com>
2022-11-20 00:34:04 +0100

    First project build
    Added DEBUGS macro to use for priniting logs via UART.
    Created files init.c and init.h to initialize all modules there.
    Added function initializeBoard() to init all modules from main function

---
+++ Core/Inc/init.h
@@ -0,0 +1,13 @@
+/*
+ * init.h
+ *
+ *  Created on: Nov 20, 2022
+ *      Author: Pawel
+ */
+
+#ifndef INC_INIT_H_
+#define INC_INIT_H_
+
+void initializeBoard(void);
+
+#endif /* INC_INIT_H_ */
---
+++ Core/Src/main.c
@@ -0,0 +1,37 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "init.h"
+
+int main(void)
+{
+  /* USER CODE BEGIN 2 */
+  initializeBoard();
+  /* USER CODE END 2 */
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
